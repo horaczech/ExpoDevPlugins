0f788c413a7a6f5ab99191a5cb2758a54db998c7
Petr Horacek <p3trh0r4c3k@gmail.com>
2024-06-25 15:48:47 +0200

    graphql apollo dev tools

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ScrollView, StyleSheet, Text, View, Image} from "react-native";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { useApolloClientDevTools } from "@dev-plugins/apollo-client";
+
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
 }
 
+const Main = () => {
+  useApolloClientDevTools(client);
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  const contents = data?.locations.map(({ id, name, description, photo }) => (
+      <View key={id} style={styles.item}>
+        <Text style={styles.name}>{name}</Text>
+        <Image source={{ uri: photo }} style={styles.photo} />
+        <Text style={styles.aboutCaption}>About this location:</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+  ));
+
+  return <ScrollView>{contents}</ScrollView>;
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  item: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  aboutCaption: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginTop: 10,
+    marginBottom: 5
+  }
 });
